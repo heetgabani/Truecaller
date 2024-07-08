5513ceb27e63c6df3a89fde7fd9da32f6b0f3727
Heet Gabani <heetgabani@192.168.1.62>
2024-07-08 15:38:35 +0530

    build

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,31 @@ import React, { useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const OTPless = Reflect.get(window, "OTPless");
-
-  const callback = console.log;
-
-  const OTPlessSignin = new OTPless(callback);
-
-  const handleTrueCaller = async () => {
-    OTPlessSignin.initiate({
-      channel: "OAUTH",
-      channelType: "TRUE_CALLER",
-    }).then(console.log);
-  };
-
   useEffect(() => {
+    const OTPless = Reflect.get(window, "OTPless");
+
+    const callback = console.log;
+
+    const OTPlessSignin = new OTPless(callback);
+
+    const handleTrueCaller = async () => {
+      OTPlessSignin.initiate({
+        channel: "OAUTH",
+        channelType: "TRUE_CALLER",
+      }).then(console.log);
+    };
+
     const timer = setTimeout(() => {
       handleTrueCaller();
-    }, 10000);
+    }, 10000); // 10 seconds
 
     return () => clearTimeout(timer);
   }, []);
-
-  return <div className="App">Truecaller Demo</div>;
+  return (
+    <div className="App">
+      <h1>OTPless TrueCaller Demo</h1>
+    </div>
+  );
 }
 
 export default App;
